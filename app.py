76831cab42c24e65ddc032d057ebe7c1534bdda8
Company <manideepmadhala123@gmail.com>
2023-12-19 14:28:22 +0530

    Fuctions

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("My name is ,manideep")
+list = ["Mani", "Deep", "Manideep"]
+listnumbers = [1, 2, 3, 4, 5]
+lucky = listnumbers.copy()
+print(lucky)
+
+tuple = [("dharani"), ("dharu")]
+print(tuple[1])
+
+def my_function(Name ,age):
+    print("Hello " + Name + "Your age : "+age)
+
+
+my_function("Dharani ", "24")
+my_function("Sreeja ", "25")
