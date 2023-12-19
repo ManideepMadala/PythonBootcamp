0d7015d8cb0fe73a5c17cd0a58cbb467c0fa7646
Company <manideepmadhala123@gmail.com>
2023-12-19 22:30:07 +0530

    If Conditions

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-list = ["Mani", "Deep", "Manideep"]
-listnumbers = [1, 2, 3, 4, 5]
-lucky = listnumbers.copy()
-print(lucky)
-
-tuple = [("dharani"), ("dharu")]
-print(tuple[1])
-
-def my_function(Name ,age):
-    print("Hello " + Name + "Your age : "+age)
-
-
-my_function("Dharani ", "24")
-my_function("Sreeja ", "25")
+is_male = False
+is_tall = True
+if is_male or is_tall:
+    print("You are a male or tall or both")
+elif is_male and not (is_tall):
+    print("you are a short male")
+elif is_male and not (is_tall):
+    print("you are not a male are tall")
+else:
+    print("You are neither male nor tall")
+def max_num(num1, num2, num3):
+    if num1!=num2 and num1>=num3:
+        return num1
+    elif num2>=num1 and num2>=num3:
+        return num2
+    else:
+        return num3
+print((max_num(3, 49, 20)))
